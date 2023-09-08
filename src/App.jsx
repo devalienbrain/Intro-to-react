7bd25b26c54d0f5bd0111c78c7f39bd076f7801f
Alien Brain <murad.dub@gmail.com>
2023-09-08 14:47:00 +0600

    React like thinking

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Things to know about React</h3>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </>
+  )
+}
+
+export default App
